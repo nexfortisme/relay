944353dd90b038e311735e1b6ce76b1b012ddc43
David <nexfortisme+github@pm.me>
2026-04-24 09:25:20 -0400

    Simple cron heartbeat loop for backend

--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,5 @@
 module github.com/nexfortisme/relay
 
 go 1.25.5
+
+require github.com/robfig/cron/v3 v3.0.0 // indirect
--- api/main.go
+++ api/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/robfig/cron/v3"
+)
 
 func main() {
-	fmt.Println("Hello, World! 2")
+	fmt.Println("Hello, World!")
+
+	c := cron.New(cron.WithSeconds())
+	c.AddFunc("*/2 * * * * *", func() {
+		fmt.Println("Heartbeat", time.Now().Format(time.RFC3339))
+		heartbeat()
+	})
+
+	fmt.Println("Starting cron")
+	c.Start()
+	defer c.Stop()
+
+	select {}
+}
+
+func heartbeat() {
+	fmt.Println("Heartbeat")
 }
